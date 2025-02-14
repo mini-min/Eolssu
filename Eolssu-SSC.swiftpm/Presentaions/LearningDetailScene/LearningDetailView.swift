5164d55880eb28b7cc742f585a463e46b970916d
mini-min <2alswo7@khu.ac.kr>
2025-02-14 20:49:21 +0900

    Implementaion LearningDetailView

--- Eolssu-SSC.swiftpm/Presentaions/LearningDetailScene/LearningDetailView.swift
+++ Eolssu-SSC.swiftpm/Presentaions/LearningDetailScene/LearningDetailView.swift
@@ -5,11 +5,10 @@ struct LearningDetailView: View {
     @State private var isShowPlayAlert: Bool = false
     @State private var isPlaying: Bool = false
     @State private var currentBeatIndex: Int = 0
+    @State private var currentRhythm: RhythmType = .rest
     @State private var repeatValue: Int = 0
     @State private var countdownValue: Int? = nil
-    @State private var playingJangdans: [RhythmType] = []
-    @State private var playingScore: Score = .good
-    
+        
     @Binding var isDetailViewVisible: Bool
     var jangdan: JangDanType
     
@@ -86,25 +85,14 @@ struct LearningDetailView: View {
                     }
                     .animation(.easeInOut(duration: 0.3), value: repeatValue)
                     
-                    PerformRhythmView(repeatValue: $repeatValue)
-//                    HStack(alignment: .center, spacing: 130) {
-//                        ForEach(RhythmType.allCases.dropLast(), id: \.self) { rhythm in
-//                            Button(action: {
-//                                SoundManager.shared.playSound(name: rhythm.sound)
-//                                soundRhythmImageTapped(rhythm)
-//                            }) {
-//                                rhythm.letterImage
-//                                    .scaleEffect(1.8)
-//                            }
-//                        }
-//                    }
+                    PerformRhythmView(
+                        repeatValue: $repeatValue, 
+                        currentRhythm: $currentRhythm
+                    )
+                    .background(Color.white)
+                    .cornerRadius(20)
                 }
                 .padding(.bottom, 80)
-                                
-                ScoreView(scoreStatus: $playingScore)
-                    .cornerRadius(20)
-                    .frame(height: 120)
-                    .padding(.horizontal, 50)
             }
             
             if let countdownValue {
@@ -163,13 +151,12 @@ extension LearningDetailView {
     func startPlaying() {
         isPlaying = true
         currentBeatIndex = 0
-        playingJangdans = []
         
         let interval = Double(60) / Double(jangdan.tempo.bpm)
-        
         if repeatValue % 2 != 0 {
             SoundManager.shared.playSound(name: jangdan.rhythms[0].sound)
         }
+        currentRhythm = jangdan.rhythms[0]
         
         Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { timer in
             if !isPlaying {
@@ -180,7 +167,6 @@ extension LearningDetailView {
             if currentBeatIndex >= jangdan.rhythms.count - 1 {
                 currentBeatIndex = 0
                 repeatValue += 1
-                playingJangdans = []
                 if repeatValue % 2 != 0 {
                     SoundManager.shared.playSound(name: jangdan.rhythms[currentBeatIndex].sound)
                 }
@@ -190,34 +176,14 @@ extension LearningDetailView {
                     SoundManager.shared.playSound(name: jangdan.rhythms[currentBeatIndex].sound)
                 }
             }
+            currentRhythm = jangdan.rhythms[currentBeatIndex]
         }
     }
     
     func stopPlaying() {
         isPlaying = false
         repeatValue = 0
-    }
-    
-    func compareArrays() {
-        let matchingScore = zip(jangdan.rhythms, playingJangdans).filter { $0 == $1 }.count
-        
-        if matchingScore >= jangdan.rhythms.count - 1 {
-            playingScore = .perfect
-        } else if matchingScore >= jangdan.rhythms.count / 2 - 1 {
-            playingScore = .good
-        } else {
-            playingScore = .oops
-        }
-    }
-    
-    func soundRhythmImageTapped(_ rhythm: RhythmType) {
-        if repeatValue % 2 == 0 && currentBeatIndex < jangdan.rhythms.count {
-            if playingJangdans.count > currentBeatIndex {
-                playingJangdans[currentBeatIndex] = rhythm // 이미 값이 있으면 덮어쓰기
-            } else {
-                playingJangdans.append(rhythm) // 값이 없으면 추가
-            }
-        }
+        currentRhythm = .rest
     }
 }
 
--- Eolssu-SSC.swiftpm/Presentaions/LearningDetailScene/PerformRhythmView.swift
+++ Eolssu-SSC.swiftpm/Presentaions/LearningDetailScene/PerformRhythmView.swift
@@ -1,34 +1,48 @@
 import SwiftUI
 
-struct PerformRhythmView: View {
-    @State private var performRhythm: RhythmType = .rest
+struct PerformRhythmView: View {    
     @Binding var repeatValue: Int
-    
+    @Binding var currentRhythm: RhythmType
+        
     var body: some View {
-        HStack(alignment: .center, spacing: 130) {
-            ForEach(RhythmType.allCases.dropLast(), id: \.self) { rhythm in
-                Button(action: {
-                    performRhythm = rhythm
-                    SoundManager.shared.playSound(name: rhythm.sound)
-                }) {
-                    if repeatValue % 2 != 0 {
-                        rhythm.letterImage
-                            .renderingMode(.template)
-                            .foregroundColor(.eolssuGray2)
-                            .scaleEffect(1.8)
-                            .contentShape(Rectangle())
-                    } else {
-                        rhythm.letterImage
-                            .scaleEffect(1.8)
-                            .contentShape(Rectangle())
+        ZStack {
+            Image.janggu3
+            
+            HStack(alignment: .bottom, spacing: 130) {
+                ForEach(RhythmType.allCases.dropLast(), id: \.self) { rhythm in
+                    
+                    VStack {
+                        Button(action: {
+                            SoundManager.shared.playSound(name: rhythm.sound)
+                        }) {
+                            if repeatValue % 2 != 0 {
+                                rhythm.letterImage
+                                    .renderingMode(.template)
+                                    .foregroundColor(.eolssuGray2)
+                                    .scaleEffect(1.8)
+                                    .contentShape(Rectangle())
+                            } else {
+                                rhythm.letterImage
+                                    .scaleEffect(1.8)
+                                    .contentShape(Rectangle())
+                            }
+                        }
+                        .disabled(repeatValue % 2 != 0)
+                        .padding(.bottom, 80)
+                        
+                        Text("Tap!")
+                            .font(.myFont(size: 32))
+                            .foregroundStyle(Color.eolssuPink)
+                            .opacity(repeatValue % 2 == 0 && currentRhythm == rhythm ? 1 : 0)
                     }
+                    
                 }
-                .disabled(repeatValue % 2 != 0)
             }
         }
+        .padding()
     }
 }
 
 #Preview {
-    PerformRhythmView(repeatValue: .constant(0))
+    PerformRhythmView(repeatValue: .constant(0), currentRhythm: .constant(.dung))
 }
